313cb17dc6b04112871225881251801981d87b5b
Fonsitax <mynameisfonja@gmail.com>
2024-11-16 15:09:15 +0100

    Initial commit: Set up Tailwind and Vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-gray-100">
+      Hello world!
+    </h1>
+  )
+}
